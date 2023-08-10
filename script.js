0602a1846f35bee35234ceec1e2656f93d649e34
Jana-Senpai <Janagaming935@gmail.com>
2023-08-10 11:36:23 +0800

    delete contact-section

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const cardLinks = document.querySelectorAll('.card-link'); // Memilih semua tautan dengan kelas 'card-link'
-  
-    cardLinks.forEach(link => {
-      link.addEventListener('click', (event) => {
-        event.preventDefault(); // Mencegah tautan mengarahkan langsung ke halaman
-  
-        const card = link.closest('.card'); // Mencari elemen kartu terdekat dari tautan yang diklik
-        const title = card.querySelector('.card-title').innerText;
-        const subtitle = card.querySelector('.card-subtitle').innerText;
-        const description = card.querySelector('.card-text').innerText;
-        const price = card.querySelector('.card-footer p').innerText;
-  
-        // Mengubah URL halaman dengan parameter data
-        const detailUrl = `detail.html?title=${encodeURIComponent(title)}&subtitle=${encodeURIComponent(subtitle)}&description=${encodeURIComponent(description)}&price=${encodeURIComponent(price)}`;
-        window.location.href = detailUrl;
-      });
-    });
-  });
+// Scroll Reveal
+ScrollReveal({
+    // reset: true,
+    distance: "200px",
+    duration: 1500,
+    delay: 200,
+    reset: true
+});
+
+ScrollReveal().reveal(".fromLeft", {origin:"left"});
+ScrollReveal().reveal(".fromRight", {origin:"right"});
+ScrollReveal().reveal(".fromBottom", {origin:"bottom"});
+ScrollReveal().reveal(".fromTop", {origin:"top"});
