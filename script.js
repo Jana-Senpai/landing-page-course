7b19600b456065b442371cea81201887251a5517
Jana-Senpai <Janagaming935@gmail.com>
2023-08-11 10:13:54 +0800

    add footer with google map, navigation, and logo

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ ScrollReveal().reveal(".fromLeft", {origin:"left"});
 ScrollReveal().reveal(".fromRight", {origin:"right"});
 ScrollReveal().reveal(".fromBottom", {origin:"bottom"});
 ScrollReveal().reveal(".fromTop", {origin:"top"});
+
+// Navbar scroll effect
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+        navbar.style.backgroundColor = "rgba(33, 37, 41, 0.9)";
+        navbar.style.borderBottom = "1px solid #fff";
+    } else {
+        navbar.style.backgroundColor = "rgba(33, 37, 41, 1)";
+        navbar.style.borderBottom = "none";
+    }
+});
